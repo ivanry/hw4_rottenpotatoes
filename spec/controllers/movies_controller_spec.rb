3e970ab6bc0f1a0da08db07c1d54a509171e4d49
ivan.rybnikov <ivan.rybnikov@gmail.com>
2013-02-16 23:34:35 +0300

    next step

---
+++ spec/controllers/movies_controller_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe MoviesController do
+  describe 'Find Movies With Same Director' do
+    it ''
+    it 'receive click on "Find Movies With Same Director" and grab the id'
+  end
+end
---
+++ spec/models/movie_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe Movie do
+  it 'should find movies by director'
+
+end

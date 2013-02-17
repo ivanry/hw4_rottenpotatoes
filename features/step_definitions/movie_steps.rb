536c75d327b31d8897f8f5855795bcdd333c8103
ivan.rybnikov <ivan.rybnikov@gmail.com>
2013-02-17 14:17:06 +0300

    Add cucumber tests from hw3 to get 100% coverage of controllers.

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -7,3 +7,20 @@ end
 
 Then /^the director of "([^"]*)" should be "([^"]*)"$/ do |movie_title, director|
 end
+
+Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+#And /^I should see "([^"]*)" before "([^"]*)"$/ do |phrase_1, phrase_2|
+  begin
+    e1 = Date.parse(e1).to_s
+  rescue ArgumentError => e
+  end
+
+  begin
+    e2 = Date.parse(e2).to_s
+  rescue ArgumentError => e
+  end
+
+  index1 = page.body.index(e1)
+  index2 = page.body.index(e2)
+  index1.should < index2
+end

ec3534809546b5b1b85de0410a56a61443a5ee45
ivan.rybnikov <ivan.rybnikov@gmail.com>
2013-02-27 00:34:16 +0300

    field validation added on UI

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,17 @@
 class Movie < ActiveRecord::Base
+  RATINGS = %w[G PG PG-13 R NC-17]  #  %w[] shortcut for array of strings
+  validates :title, :presence => true
+  validates :release_date, :presence => true
+  validate :released_1930_or_later # uses custom validator below
+  validates :rating, :inclusion => {:in => RATINGS}, :unless => :grandfathered?
+  def released_1930_or_later
+    errors.add(:release_date, 'must be 1930 or later') if
+        self.release_date < Date.parse('1 Jan 1930')
+  end
+
+  @@grandfathered_date = Date.parse('1 Jan 1930')
+  def grandfathered? ; self.release_date >= @@grandfathered_date ; end
+
   def self.all_ratings
     %w(G PG PG-13 NC-17 R)
   end

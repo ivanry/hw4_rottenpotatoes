3b691a5dde6eeb9196975a6d493959cc3d305250
ivan.rybnikov <ivan.rybnikov@gmail.com>
2013-03-01 15:39:05 +0300

    validation vor DB associations added

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,12 @@ class Review < ActiveRecord::Base
   belongs_to :movie
   belongs_to :moviegoer
   attr_protected :moviegoer_id # see text
+
+  # review is valid only if it's associated with a movie:
+  validates :movie_id, :presence => true
+  # can ALSO require that the referenced movie itself be valid
+  #  in order for the review to be valid:
+  validates_associated :movie
+
   #attr_protected :movie_id # see text
 end

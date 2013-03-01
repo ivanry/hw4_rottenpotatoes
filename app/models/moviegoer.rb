ecc4db428f2f7377f341d0bcfe83a556f41804fc
ivan.rybnikov <ivan.rybnikov@gmail.com>
2013-03-01 15:24:23 +0300

    "through" association example

--- app/models/moviegoer.rb
+++ app/models/moviegoer.rb
@@ -1,6 +1,7 @@
 # Edit app/models/moviegoer.rb to look like this:
 class Moviegoer < ActiveRecord::Base
   has_many :reviews
+  has_many :movies, :through => :reviews
 
   include ActiveModel::MassAssignmentSecurity
   attr_protected :uid, :provider, :name # see text for explanation
